d918333ecd82ced4c8eadd1e436b71999338c420
Josef Vavra <vavra.pepa@gmail.com>
2023-02-27 10:50:00 +0100

    open map on start

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local EventFrame = CreateFrame("frame", "EventFrame")
 EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 EventFrame:SetScript("OnEvent", function(self, event, ...)
-	if(event == "PLAYER_ENTERING_WORLD") then
-		C_Map.OpenWorldMapInteraction()
+	if event == "PLAYER_ENTERING_WORLD" then
+        OpenWorldMap();
 	end
 end)

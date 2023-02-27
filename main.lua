0c76adb28c23d919a595de780ecb0b30ba3ca966
Josef Vavra <vavra.pepa@gmail.com>
2023-02-27 09:27:38 +0100

    enter world event register

---
+++ main.lua
@@ -0,0 +1,8 @@
+local EventFrame = CreateFrame("frame", "EventFrame")
+EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+EventFrame:SetScript("OnEvent", function(self, event, ...)
+	if(event == "PLAYER_ENTERING_WORLD") then
+		ChatFrame1:Hide()
+	end
+end)
